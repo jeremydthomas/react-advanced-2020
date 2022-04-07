1aaf4ce9dde1a949b07631f25cee9567bb30cd5f
jeremydthomas <tommyhilfiger12@hotmail.com>
2022-04-06 23:38:39 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // import React from 'react'
-import Setup from './tutorial/1-useState/setup/4-useState-object';
+import Setup from './tutorial/1-useState/setup/5-useState-counter';
 function App() {
 	return (
-		<div className="container">
+		<div className='container'>
 			<Setup />
 		</div>
 	);
